7a95a1f39eb11e0f335d29f8abb91fa1e4420690
achapadev <83032999+achapadev@users.noreply.github.com>
2021-04-23 02:43:35 -0500

    used javascript for src of image

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//variable to hold random photo from picsum
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
-    <h1 className="heading" contentEditable="true" spellCheck="false">
-      My Favourite Foods
-    </h1>
-    <div>
-      <img
-        className="food-img"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoAUb9xlu8HbqDL96b-C5zegaWEgXGhp2gqQ&usqp=CAU"
-      />
-      <img
-        className="food-img"
-        src="https://store.anxodc.com/wp-content/uploads/2020/06/image-asset-3.jpeg"
-      />
-      <img
-        className="food-img"
-        src="https://www.washingtonpost.com/news/voraciously/wp-content/uploads/sites/68/2020/01/v-rr-lunarnewyear_01_leadweb.jpg"
-      />
-    </div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img alt="random" src={img + "?grayscale"} />
+    <div></div>
   </div>,
   document.getElementById("root")
 );
